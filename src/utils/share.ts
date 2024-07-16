99b5f4255cfc6c9502fabcc27bcbf5e4873932f3
Fedor Rychkov <fedorrychkov@yandex.ru>
2024-07-17 00:11:47 +0300

    fix: fix share links

--- src/utils/share.ts
+++ src/utils/share.ts
@@ -5,5 +5,5 @@ const utils = initUtils()
 export const shareTgLink = (path: string) => {
   const payload = window.btoa(path)
   const parsed = payload?.replace('=', '')?.replace('=', '')
-  utils.openTelegramLink(`https://t.me/share/url?url=https://t.me/personal_wish_list_bot?start=${parsed}`)
+  utils.shareURL(`https://t.me/personal_wish_list_bot?start=${parsed}`)
 }
